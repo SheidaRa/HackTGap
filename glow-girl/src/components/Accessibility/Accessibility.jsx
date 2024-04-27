621b7ddfc49be1dc92f6384f8419cd001447d340
traceytreat <treatracey@gmail.com>
2024-04-27 16:20:23 -0500

    Change size of accessibility button.

--- glow-girl/src/components/Accessibility/Accessibility.jsx
+++ glow-girl/src/components/Accessibility/Accessibility.jsx
@@ -5,7 +5,7 @@ function Accessibility() {
         /*on click this should open up a menu with accessibility options*/
         /*ideally the size param should be set to a variable whose value is set in the access. menu*/
         <div id="accessibilityButton">
-            <IoAccessibility size={100} />
+            <IoAccessibility size={80} />
         </div>
     );
 }

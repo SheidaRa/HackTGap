0fe7d5cc404d654b11b9dc9b6962762379158297
traceytreat <treatracey@gmail.com>
2024-04-27 16:19:14 -0500

    Add css/template for an accessibility button on the bottom of the screen.

--- glow-girl/src/App.js
+++ glow-girl/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Accessibility from './components/Accessibility/Accessibility';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Accessibility />
     </div>
   );
 }
---
+++ glow-girl/src/components/Accessibility/Accessibility.jsx
@@ -0,0 +1,12 @@
+import { IoAccessibility } from "react-icons/io5";
+import './Accessibility.css'
+function Accessibility() {
+    return (
+        /*on click this should open up a menu with accessibility options*/
+        /*ideally the size param should be set to a variable whose value is set in the access. menu*/
+        <div id="accessibilityButton">
+            <IoAccessibility size={100} />
+        </div>
+    );
+}
+export default Accessibility;

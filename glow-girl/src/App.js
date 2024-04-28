41bbdf8c9b9af41def1be9860d1e4e5fef98b983
Liz Kerber <emkerber@outlook.com>
2024-04-27 23:16:15 -0500

    Remove unused dependency

--- glow-girl/src/App.js
+++ glow-girl/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Accessibility from './components/Accessibility/Accessibility';
 import Categories from './components/Categories/Categories';

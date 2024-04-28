829d12fe1f8dfd66c3e285ec92719160e98d60cd
Liz Kerber <emkerber@outlook.com>
2024-04-27 23:10:06 -0500

    Set up client-side routing

--- glow-girl/src/App.js
+++ glow-girl/src/App.js
@@ -4,13 +4,27 @@ import './App.css';
 import Accessibility from './components/Accessibility/Accessibility';
 import Categories from './components/Categories/Categories';
 import CategoryPage from './components/CategoryPage/CategoryPage';
+import {
+  HashRouter as Router,
+  Route,
+  Routes,
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Categories />
-      <Accessibility />
-      <CategoryPage />
+      <Router>
+        <Routes>
+
+          <Route exact path="/" element={<Categories />} />
+
+          {/* <Accessibility /> */}
+          {/* TODO move Categories and Accessibility into Home component */}
+          
+          <Route path="/category" element={<CategoryPage />} />
+
+        </Routes>
+      </Router>
     </div>
   );
 }
--- glow-girl/src/components/CategoryListItem/CategoryListItem.jsx
+++ glow-girl/src/components/CategoryListItem/CategoryListItem.jsx
@@ -1,15 +1,18 @@
 // TODO import for client-side routing (useHistory?)
 import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 function CategoryListItem({ category }) {
 
     const dispatch = useDispatch();
+    const navigate = useNavigate();
 
     let goToCategoryPage = () => {
         // save category obj to reducer
         dispatch({ type: 'SET_CATEGORY', payload: category });
 
-        // TODO route user to CategoryPage
+        // route user to CategoryPage
+        navigate('/category');
     }
 
     return (
